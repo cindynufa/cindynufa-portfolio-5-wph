a87b5c4bcd60e1c57d3c1a9e719455b96e92646a
Cindy Nufa <cindynufa@gmail.com>
2025-11-11 23:01:19 +0700

    Feat: added component waves for element waves

---
+++ src/components/waves/index.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Waves() {
+  return (
+    <div className="relatives lg:top-136">
+      <Image src="/images/waves.png" alt="Waves" width={1900} height={381} />
+    </div>
+  );
+}
