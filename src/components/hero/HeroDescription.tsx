12a46f9a4ba17852d0e7cc012d2a8d0cc587e8eb
Cindy Nufa <cindynufa@gmail.com>
2025-11-11 23:03:38 +0700

    Feat: edited name at description, removed waves from index file

--- src/components/hero/HeroDescription.tsx
+++ src/components/hero/HeroDescription.tsx
@@ -1,8 +1,8 @@
 export default function HeroDescription() {
   return (
     <p className="text-md font-semibold text-center text-neutral-200">
-      Hi, I’m Alex, a passionate web developer with over{' '}
-      <span className="text-white">3 years of experience</span> 3 years of
+      Hi, I’m Cindy, a passionate web developer with over{' '}
+      <span className="font-bold text-white">3 years of experience</span> 3 years of
       experience in creating responsive websites. I also teach aspiring
       developers to master modern web programming and bring their ideas to life.
     </p>
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -5,12 +5,14 @@ import { Button } from '../ui/button';
 
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col w-full items-center">
       <div className="w-957 h-auto mt-73">
         <HeroButtonPortfolio />
         <HeroHeading />
-        <HeroDescription/>
-        <Button className='w-51.5 h-12 p-2 gap-1.5 text-md font-medium text-neutral-100'>View Portfolio</Button>
+        <HeroDescription />
+        <Button className="w-51.5 h-12 p-2 gap-1.5 text-md font-medium text-neutral-100">
+          View Portfolio
+        </Button>
       </div>
     </div>
   );
