72cce5e714a60f1bf4759a366be6d6955b90f345
Cindy Nufa <cindynufa@gmail.com>
2025-11-11 22:57:44 +0700

    Feat: added bground

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,7 @@ import NavMenu from './NavMenu';
 
 export default function Header() {
   return (
-    <header className='fixed flex z-50 w-full h-21 gap-225 px-35 justify-between items-center'>
+    <header className='fixed flex z-50 w-full h-21 gap-225 px-35 justify-between items-center bg-black'>
       <Logo />
       <NavMenu/>
       <Button className='w-45 h-12 p-2 gap-1.5 text-md font-medium text-neutral-100'><Mail/>Hire Me</Button>
