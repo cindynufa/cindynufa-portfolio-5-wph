0e9013978bca4333b52524b60cd537f97ad870e6
Cindy Nufa <cindynufa@gmail.com>
2025-11-13 22:13:29 +0700

    Fix: fixed rotate for Detail Button

--- src/components/portfolio/PortfolioDetailButton.tsx
+++ src/components/portfolio/PortfolioDetailButton.tsx
@@ -1,7 +1,7 @@
-
-
-export default function PortfolioDetailButton(){
-    return(
-        <button className="w-25 h-25 border outline-gradient bg-neutral-500 text-md font-semibold text-neutral-100">Detail</button>
-    )
+export default function PortfolioDetailButton() {
+  return (
+    <button className="w-25 h-25 border outline-gradient bg-neutral-500 text-md font-semibold text-neutral-100 -rotate-10">
+      Detail
+    </button>
+  );
 }
