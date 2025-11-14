048cc6f3a851cbd294668d8fcdde235116b7e346
Cindy Nufa <cindynufa@gmail.com>
2025-11-15 00:01:09 +0700

    Feat: added link to project section

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -2,19 +2,22 @@ import HeroHeading from './HeroHeading';
 import HeroButtonPortfolio from './HeroButtonPortfolio';
 import HeroDescription from './HeroDescription';
 import { Button } from '../ui/button';
+import Link from 'next/link';
 
 export default function Hero() {
   return (
-    <section id='home' className="w-full items-center">
+    <section id="home" className="w-full items-center">
       <div className="flex flex-col w-957 h-auto mx-auto pt-273 gap-5">
         <HeroButtonPortfolio />
         <HeroHeading />
         <HeroDescription />
-        <div className="text-center">
-          <Button className="w-51.5 h-12 p-2 gap-1.5 text-md font-medium text-neutral-100">
-            View Portfolio
-          </Button>
-        </div>
+        <Link href="#projects">
+          <div className="text-center">
+            <Button className="w-51.5 h-12 p-2 gap-1.5 text-md font-medium text-neutral-100">
+              View Portfolio
+            </Button>
+          </div>
+        </Link>
       </div>
     </section>
   );
