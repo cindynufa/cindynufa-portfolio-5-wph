55f6c7dedd6866467809abf7e309cb11dbc5aa43
Cindy Nufa <cindynufa@gmail.com>
2025-11-15 00:02:49 +0700

    Feat: added link to contact section

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,16 +4,19 @@ import { Mail } from 'lucide-react';
 import { Button } from '../ui/button';
 import Logo from './Logo';
 import NavMenu from './NavMenu';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="fixed flex z-50 w-full h-21 gap-225 px-35 justify-between items-center bg-neutral-600">
       <Logo />
       <NavMenu />
-      <Button className="w-45 h-12 p-2 gap-1.5 text-md font-medium text-neutral-100">
-        <Mail />
-        Hire Me
-      </Button>
+      <Link href="#contact">
+        <Button className="w-45 h-12 p-2 gap-1.5 text-md font-medium text-neutral-100">
+          <Mail />
+          Hire Me
+        </Button>
+      </Link>
     </header>
   );
 }
