489d41d609bde729dd088c90e4f2b181a4744d83
Cindy Nufa <cindynufa@gmail.com>
2025-11-14 17:11:03 +0700

    Fix: fixed the position of the text

--- src/components/about/AboutCardItem.tsx
+++ src/components/about/AboutCardItem.tsx
@@ -5,13 +5,11 @@ interface AboutCardItemProps {
 
 export default function AboutCardItem({ heading, text }: AboutCardItemProps) {
   return (
-    <div className="about-card outline-gradient">
-      <div className="flex flex-col w-full translate-y-1/2 items-center">
-        <h3 className="text-display-2xl font-extrabold text-neutral-100">
-          {heading}
-        </h3>
-        <p className="text-md font-regular text-neutral-100">{text}</p>
-      </div>
+    <div className="flex flex-col justify-center items-center about-card outline-gradient">
+      <h3 className="text-display-2xl font-extrabold text-neutral-100">
+        {heading}
+      </h3>
+      <p className="text-md font-regular text-neutral-100">{text}</p>
     </div>
   );
 }
