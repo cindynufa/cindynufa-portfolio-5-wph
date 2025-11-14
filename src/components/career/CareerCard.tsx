55209048512d89c232979a8e2cb9d122352640c0
Cindy Nufa <cindynufa@gmail.com>
2025-11-14 13:30:07 +0700

    Feat: added Career Card Line component for Career Section

--- src/components/career/CareerCard.tsx
+++ src/components/career/CareerCard.tsx
@@ -2,32 +2,39 @@ import CareerCardConst from '@/constants/CareerCard';
 import CareerCardItem from './CareerCardItem';
 import { Fragment } from 'react/jsx-runtime';
 import { Building, CalendarDays, Dot } from 'lucide-react';
+import CareerCardLine from './CareerCardLine';
 
 export default function CareerCard() {
   return (
-    <div className="flex flex-col w-full gap-6">
-      {CareerCardConst.map((careerCard, index) => (
-        <div
-          key={index}
-          className="flex flex-col w-full p-6 gap-5 border-0 bg-neutral-500 rounded-3xl"
-        >
-          <h3 className="text-display-xs font-bold text-white">
-            {careerCard.heading}
-          </h3>
-          <div className="flex flex-row gap-3">
-            <Building />
-            <p>{careerCard.text}</p>
-            <Dot />
-            <CalendarDays />
-            <p>{careerCard.date}</p>
+    <div className="flex flex-row w-full gap-6">
+      <CareerCardLine />
+      <div className="flex flex-col w-full gap-6">
+        {CareerCardConst.map((careerCard, index) => (
+          <div
+            key={index}
+            className="flex flex-col w-full p-6 gap-5 border-0 bg-neutral-500 rounded-3xl"
+          >
+            <h3 className="text-display-xs font-bold text-white">
+              {careerCard.heading}
+            </h3>
+            <div className="flex flex-row gap-3">
+              <Building />
+              <p>{careerCard.text}</p>
+              <Dot />
+              <CalendarDays />
+              <p>{careerCard.date}</p>
+            </div>
+            {careerCard.description.map((description, index1) => (
+              <Fragment key={index1}>
+                <CareerCardItem
+                  src={careerCard.src}
+                  description={description}
+                />
+              </Fragment>
+            ))}
           </div>
-          {careerCard.description.map((description, index1) => (
-            <Fragment key={index1}>
-              <CareerCardItem src={careerCard.src} description={description} />
-            </Fragment>
-          ))}
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/career/CareerCardLine.tsx
@@ -0,0 +1,11 @@
+export default function CareerCardLine() {
+  return (
+    <div className="w-fit line translate-y-70 -ml-70">
+      <div className="relative flex flex-col w-5 h-5 border-6 rounded-full justify-between">
+        <div className="absolute line-circle"></div>
+        <div className="absolute line-circle"></div>
+        <div className="absolute line-circle"></div>
+      </div>
+    </div>
+  );
+}
