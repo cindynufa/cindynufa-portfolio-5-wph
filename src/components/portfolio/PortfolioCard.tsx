030c4069ac742d96d630da666a15126be9eb3c03
Cindy Nufa <cindynufa@gmail.com>
2025-11-14 17:15:06 +0700

    Fix: fixed size of portfolio pictures, and position of the Detail button

--- src/components/portfolio/PortfolioCard.tsx
+++ src/components/portfolio/PortfolioCard.tsx
@@ -7,40 +7,44 @@ import PortfolioDetailButton from './PortfolioDetailButton';
 
 export default function PortfolioCard() {
   return (
-    <div className="grid grid-cols-3 w-full gap-12 justify-between">
-      {PortfolioConst.map((portfolioCard, index) => (
-        <div key={index} className="flex flex-col w-full gap-6">
-          <Image
-            src={portfolioCard.src}
-            alt="Portfolio"
-            width={370}
-            height={370}
-            className="rounded-3xl"
-          />
-          <div className="flex flex-col w-full gap-10">
-            <div className="flex flex-col gap-3">
-              <h4 className="text-xl font-bold text-neutral-100">
-                {portfolioCard.heading}
-              </h4>
-              <div className="flex flex-row gap-2">
-                {portfolioCard.language.map((language, index1) => (
-                  <Fragment key={index1}>
-                    <PortfolioCardLanguage language={language} />
-                  </Fragment>
-                ))}
+    <div className="relative w-full">
+      <div className="grid grid-cols-3 gap-12 justify-between">
+        {PortfolioConst.map((portfolioCard, index) => (
+          <div key={index} className="flex flex-col w-full gap-6">
+            <Image
+              src={portfolioCard.src}
+              alt="Portfolio"
+              width={370}
+              height={370}
+              className="w-full h-auto rounded-3xl"
+            />
+            <div className="flex flex-col w-full gap-10">
+              <div className="flex flex-col gap-3">
+                <h4 className="text-xl font-bold text-neutral-100">
+                  {portfolioCard.heading}
+                </h4>
+                <div className="flex flex-row gap-2">
+                  {portfolioCard.language.map((language, index1) => (
+                    <Fragment key={index1}>
+                      <PortfolioCardLanguage language={language} />
+                    </Fragment>
+                  ))}
+                </div>
+                <p className="text-md font-regular text-neutral-200">
+                  {portfolioCard.text}
+                </p>
               </div>
-              <p className="text-md font-regular text-neutral-200">
-                {portfolioCard.text}
-              </p>
+              <div className="flex flex-row gap-1.5">
+                <button className="text-lg font-bold text-gradient">
+                  {portfolioCard.button}
+                </button>
+                <ArrowUpRight className="text-white" />
+              </div>
             </div>
-            <button className="flex flex-row gap-1.5 text-lg font-bold text-gradient">
-              {portfolioCard.button}
-              <ArrowUpRight className="text-white" />
-            </button>
           </div>
-        </div>
-      ))}
-      <div className="w-full -mt-300 justify-center">
+        ))}
+      </div>
+      <div className="absolute left-1/2 top-[20%]">
         <PortfolioDetailButton />
       </div>
     </div>
